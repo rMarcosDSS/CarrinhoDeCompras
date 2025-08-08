592e29f1bc69c9af21ae679a8e68d2fd6e7150ef
rMarcosDSS <marcos.santos741e@gmail.com>
2025-08-08 02:47:46 -0300

    exibição de itens no carrinho

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,33 @@
+import  useStockCart  from '../hooks/useStockCart';
+
+
 export default function Cart() {
+
+const { items } = useStockCart()
+  
+  
   return (
     <section>
       <h1>Carrinho de Compras</h1>
+
+      <div>
+          {
+            items.map( (item) => (
+              <ul>
+                <li> 
+                  {item.title}
+                  <img src={item.image} alt="" />
+
+                  <button>-</button>
+                  <p>1</p>
+                  <button>+</button>
+
+                  <button>X</button>
+                </li>
+              </ul>
+            ))
+          }
+      </div>
     </section>
   );
 }
